31f1d87b5c94eb4d4e927da952c596b788b96588
Simo680 <erik.simonyan2005@gmail.com>
2024-01-29 20:02:05 +0300

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div  class="todo_item" @click="removeTodo" @dblclick="changeTodo">
+       <h1>{{ title }}</h1>
+</div>
+  
+</template>
+
+<script>
+export default {
+  props: ['title', 'completed', 'id'],
+  emits: ['changeTodo', 'removeTodo'],
+  methods: {
+    changeTodo() {
+      this.$emit('changeTodo', this.id)
+    },
+    removeTodo() {
+      this.$emit('removeTodo', this.id)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
